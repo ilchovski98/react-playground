c968fb27f557d2ef8b31e0d11f5e8ff1eeef5ff5
ilchovski98 <ilchovski98@gmail.com>
2022-12-03 16:05:56 +0200

    Add profileCard styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      <h1>Personal Digital Assistants</h1>
+      <div className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </div>
 
-      <ProfileCard title="Alexa" handle="@alexa" />
-      <ProfileCard title="SpaceX" handle="@elonboy69420" />
-      <ProfileCard title="Shopify" handle="@shopify" />
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and it helps you buy things"
+              />
+            </div>
+
+            <div className="column is-3">
+              <ProfileCard
+                title="SpaceX"
+                handle="@elonboy69420"
+                image={CortanaImage}
+                description="SpaceX was created by Elon Musk and it helps us become multiplanetary species"
+              />
+            </div>
+
+            <div className="column is-3">
+              <ProfileCard
+                title="Shopify"
+                handle="@shopify"
+                image={SiriImage}
+                description="Shopify is an ecommerce platform that makes merchants create stores easily"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
