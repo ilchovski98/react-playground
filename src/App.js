20033a640599d0bd1c622b67596b3965196189b3
ilchovski98 <ilchovski98@gmail.com>
2022-12-05 16:25:14 +0200

    Add book app logic

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    setBooks([...books, { id: Math.round(Math.random() * 9999), title }]);
+  };
+
+  const editBook = (id, title) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBook = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className="app">
+      <BookCreate onSubmit={createBook} />
+      <BookList books={books} onEdit={editBook} onDelete={deleteBook} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BookShow.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onEdit, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleEdit = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleDeleteBook = () => {
+    onDelete(book.id);
+  };
+
+  const handleSubmit = (id, title) => {
+    onEdit(id, title);
+    handleEdit();
+  };
+
+  let content = <h3>{book.title}</h3>;
+
+  if (showEdit) {
+    content = <BookEdit book={book} onSubmit={handleSubmit} />;
+  }
+
+  return (
+    <div className="book-show">
+      {content}
+
+      <div className="actions">
+        <button className="edit" onClick={handleEdit}>
+          Edit
+        </button>
+
+        <button onClick={handleDeleteBook} className="delete">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BookShow;
