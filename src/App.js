224209ec2ba9506ba970f0a49564fa1d3500e0f9
ilchovski98 <ilchovski98@gmail.com>
2022-12-08 12:19:49 +0200

    Add tailwind css and buttons

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button primary>Button!</Button>
+      </div>
+
+      <div>
+        <Button warning>Button!2</Button>
+      </div>
+
+      <div>
+        <Button primary danger>Button!3</Button>
+      </div>
+
+      <div>
+        <Button>Button!4</Button>
+      </div>
+
+      <div>
+        <Button>Button!5</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
