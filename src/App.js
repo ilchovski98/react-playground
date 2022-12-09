621ace9525a71d75418107ca68678fe3aa0520cd
ilchovski98 <ilchovski98@gmail.com>
2022-12-09 17:18:48 +0200

    Add dropdown, close on outside click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import { useState } from 'react';
 import ButtonPage from './pages/ButtonPage';
-import Accordion from './components/Accordion';
+import AccordionPage from './pages/AccordionPage';
+import Dropdown from './components/Dropdown';
 
 function App() {
-  const items = [
-    {
-      id: 'asdf',
-      label: 'Do you know React?',
-      content: 'Of course! I have been doing it for 3 years now!'
-    },
-    {
-      id: 'rewg',
-      label: 'Is web3 the future?',
-      content: 'Are banks printing money? Do we need water in order to survive? Is Kobe the best basketball player to ever live?'
-    },
-    {
-      id: 'poqkwe',
-      label: 'Who are you?',
-      content: 'Ambitious, curious, good individual. :)'
-    }
+  const [selection, setSelection] = useState(null);
+
+  const handleSelection = (selection) => {
+    setSelection(selection);
+  };
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' }
   ];
+
   return (
-    <Accordion items={items} />
+    <>
+      <AccordionPage />
+      <ButtonPage />
+      <span>-------------------------------------</span>
+
+      <div className="flex">
+        <Dropdown options={options} value={selection} onChange={handleSelection} />
+      </div>
+    </>
   )
 }
 
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,26 @@
+import Accordion from '../components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 'asdf',
+      label: 'Do you know React?',
+      content: 'Of course! I have been doing it for 3 years now!'
+    },
+    {
+      id: 'rewg',
+      label: 'Is web3 the future?',
+      content: 'Are banks printing money? Do we need water in order to survive? Is Kobe the best basketball player to ever live?'
+    },
+    {
+      id: 'poqkwe',
+      label: 'Who are you?',
+      content: 'Ambitious, curious, good individual. :)'
+    }
+  ];
+  return (
+    <Accordion items={items} />
+  )
+}
+
+export default AccordionPage;
