8a4cb69155b3afe879173f2ad525da51d3f6f965
ilchovski98 <ilchovski98@gmail.com>
2022-12-17 16:46:35 +0200

    Make table component reusable

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,31 @@
-function Table({ data }) {
-	const renderedRows = data.map((row, index) => {
-		return (
-			<tr className="border-b" key={row.name + index}>
-				<td className="p-3">{row.name}</td>
-				<td className="p-3"><div className={`p-3 m-2 ${row.color}`}></div></td>
-				<td className="p-3">{row.score}</td>
-			</tr>
-		)
-	});
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>
+  });
+  
+  const renderedRows = data.map((row, index) => {
+    const renderedCells = config.map((column) => {
+      return <td key={column.label} className="p-3">{column.render(row)}</td>;
+    });
 
-	return <table className="table-auto border-spacing-2">
-		<thead>
-			<tr className="border-b-2">
-				<th>Fruits</th>
-				<th>Color</th>
-				<th>Score</th>
-			</tr>
-		</thead>
+    return (
+      <tr className="border-b" key={keyFn(row)}>
+        {renderedCells}
+      </tr>
+    )
+  });
 
-		<tbody>
-			{renderedRows}
-		</tbody>
-	</table>
+  return <table className="table-auto border-spacing-2">
+    <thead>
+      <tr className="border-b-2">
+        {renderedHeaders}
+      </tr>
+    </thead>
+
+    <tbody>
+      {renderedRows}
+    </tbody>
+  </table>
 }
 
 export default Table;
